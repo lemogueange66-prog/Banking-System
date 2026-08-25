c418b0f9ca0f48605bea33508d430dd1e7e306f4
kuetealine-cmyk <kuete.aline@ictuniversity.edu.cm>
2026-08-25 10:48:39 +0100

    Add files via upload

---
+++ testprogram.java
@@ -0,0 +1,8 @@
+public class testprogram {
+    public static void main(string[] args) {
+system.out.println( "Hello world!"); // used as  print f in java
+ }
+
+
+ } 
+ //Operators are used to perform mathematical functions on a device
